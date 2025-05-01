1905b54cac2e58f0243e2e148541949bf5932b6c
Piyush(piko04) <156587759+piko4@users.noreply.github.com>
2025-05-01 20:45:17 +0530

    made some modifications in buttons

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import ProductGallery from "./(user)/Components/ProductGallery";
+import ProductGallery from "./(user)/Components/ProductCard";
 import Footer_User from "./(user)/Components/Footer_User";
+import ProductCard from "./(user)/Components/ProductCard";
 
 export default function Home() {
   return (
     <>
-    <ProductGallery/>
+    <ProductCard/>
     <Footer_User/>
     </>
   );
